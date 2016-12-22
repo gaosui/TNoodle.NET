b2f5ac49f332cbea5f1bd5fbc6f8d7c1326dc9c9
Gao Sui <mark_svi@outlook.com>
2016-12-21 22:28:11 -0800

    Port 2x2 solver

--- Test/Program.cs
+++ Test/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TNoodle.min2phase;
+using TNoodle.Solvers;
 
 namespace Test
 {
@@ -11,10 +12,29 @@ namespace Test
     {
         public static void Main(string[] args)
         {
-            Search s = new Search();
-            //string state = Tools.randomCube();
-            //Console.WriteLine(state);
-            Console.WriteLine(s.solution("LFBFUBURBRLLFRBBLFRBUUFRLRRDDDDDURLUFDFRLFFBBULDUBULDD", 30, 10000, 1, 0));
+            var solver = new TwoByTwoSolver();
+            //var state = solver.randomState(new Random());
+            //var perm = new int[7];
+            //var ori = new int[7];
+            //TwoByTwoSolver.unpackPerm(state.permutation, perm);
+            //TwoByTwoSolver.unpackOrient(state.orientation, ori);
+            //foreach (var item in perm)
+            //{
+            //    Console.WriteLine("perm " + item);
+            //}
+            //foreach (var item in ori)
+            //{
+            //    Console.WriteLine("ori " + item);
+            //}
+            var perm = new int[] { 2, 1, 0, 3, 4, 5, 6 };
+            var ori = new int[] { 0, 0, 0, 0, 0, 0, 0 };
+            var state = new TwoByTwoState
+            {
+                permutation = TwoByTwoSolver.packPerm(perm),
+                orientation = TwoByTwoSolver.packOrient(ori)
+            };
+            Console.WriteLine(solver.solveIn(state, 10));
+
         }
     }
 }
